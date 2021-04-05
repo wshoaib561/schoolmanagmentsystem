a805063fd0cc08f18e0081d58a5f1ad6b80a6df8
hp <shoaibali57575@gmail.com>
2021-04-05 18:19:34 +0500

    Change in SampleClass

--- src/com/company/SampleClass.java
+++ src/com/company/SampleClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SampleClass {
     SampleClass(){
         System.out.println("Sample Class for Github");
+        System.out.println("Change in this File after Uploading project");
     }
 }
